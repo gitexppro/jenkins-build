1ff7e90e1fe400807b7465ce48eaeb42bb68ddff
gitexppro <madhu.mady04@gmail.com>
2020-05-11 23:26:36 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+# aws_access_key = "AKIA5GLTXTCAKEA2YUWG"
+# aws_secret_key = "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"
+aws_region = "us-east-1"
+vpc_cidr = "10.1.0.0/16"
+vpc_name = "TF-VPC"
+public_subnet1_cidr = "10.1.5.0/24"
+# public_subnet2_cidr = "10.1.6.0/24"
+IGW_name = "TF-AWS-IGW"
+public_subnet1_name = "SUB1"
+# public_subnet2_name = "SUB2"
+Main_Routing_Table =  "TF-Main-Routing-Table"
+
+
---
+++ variables.tf
@@ -0,0 +1,38 @@
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
+variable "aws_region" {}
+
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+
+variable "public_subnet1_cidr" {}
+
+variable "public_subnet1_name" {}
+
+variable "az1" {
+  default = "us-east-1a"
+}
+
+# variable "public_subnet2_cidr" {}
+
+variable "az2" {
+  default = "us-east-1b"
+}
+
+# variable "public_subnet2_name" {}
+
+variable "IGW_name" {}
+
+variable "Main_Routing_Table" {}
+
+
+
+
+
+
+
+
+
+
+
+
